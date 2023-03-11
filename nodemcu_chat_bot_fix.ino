eacce52bce58f69aec0df306f9e91f531668d244
Rahmat Hidayat <55992301+rahmathidayat1203@users.noreply.github.com>
2023-03-12 00:21:32 +0700

    first commit

---
+++ nodemcu_chat_bot_fix.ino
@@ -0,0 +1,49 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+// Replace with your network credentials
+const char* ssid = "Redmi 8";
+const char* password = "dayat123";
+
+// Initialize Telegram BOT
+#define BOTtoken "5785002391:AAFN6xdqaW81AD4CH4GOWLM3DJEZq8JPU1o"  // your Bot Token (Get from Botfather)
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID "6011565146"
+
+X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+void setup() {
+  Serial.begin(115200);
+  configTime(0, 0, "pool.ntp.org");      // get UTC time via NTP
+  client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
+
+  // Attempt to connect to Wifi network:
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  bot.sendMessage(CHAT_ID,"BOT IS STARTING.....","");
+  bot.sendMessage(CHAT_ID, "Halo Selamat Malam Selamat Datang Di Chat Bot Pakan Ikan,  Anda Bisa Memonitoring Waktu Makan Ikan Anda Lewat Bot Ini.Hope You Enjoying This..", "");
+
+}
+void loop(){
+}
